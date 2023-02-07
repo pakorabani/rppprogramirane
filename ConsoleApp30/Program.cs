60a33b57273da73c0c8a4bff51d1583c9368ba8b
pakorabani <n.mihajlov2019@pmg-pernik.com>
2023-02-07 12:30:55 +0200

    Hehe

---
+++ ConsoleApp30/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp30
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+            for (int i =0; i < n; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine(OptionsManager.ConuntLess100(arr));
+            Console.WriteLine(OptionsManager.MaxNum(arr));
+            arr = OptionsManager.ReverseArr(arr);
+            OptionsManager.PrintArr(arr);
+        }
+    }
+}
