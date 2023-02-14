0050a82d8ff11038e98c539353d7f305629f407d
pakorabani <n.mihajlov2019@pmg-pernik.com>
2023-02-14 13:27:22 +0200

    Lists merge

---
+++ ListTasksksksk/Remove Negative/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Remove_Negative
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+
+
+        }
+    }
+}
