fdf2e82cec60aa1d2484be5ecc6cfed19201c3c5
pakorabani <n.mihajlov2019@pmg-pernik.com>
2023-02-14 12:59:18 +0200

    Remove last int

---
+++ ListTasksksksk/ListTasksksksk/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ListTasksksksk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+            int last = nums[nums.Count-1];
+            //nums.Remove(last);
+            nums.RemoveAll(x => x == last);
+            Console.WriteLine(string.Join(' ', nums));
+        }
+    }
+}
